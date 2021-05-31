d8b88ae02d8d62cc501c84e07e7d40ecab3ba050
yoshiaki ohashi <ohashi.spdc@icloud.com>
2021-05-31 14:54:39 +0900

    q20のcse文をリファクタリングした

--- task.rb
+++ task.rb
@@ -231,17 +231,17 @@ class Zoo
   end
 
   def info_entry_fee(user)
-    case user.age 
+    zoo_entry_fee = case user.age 
     when 0..5
-      entry_fee = @entry_fee[:infant]
+      @entry_fee[:infant]
     when 6..12
-      entry_fee = @entry_fee[:children]
+      @entry_fee[:children]
     when 13..64
-      entry_fee = @entry_fee[:adult]
+      @entry_fee[:adult]
     when 65..120
-      entry_fee = @entry_fee[:senior]
+      @entry_fee[:senior]
     end
-    puts "#{user.name}さんの入場料金は#{entry_fee}円です"
+    puts "#{user.name}さんの入場料金は#{zoo_entry_fee}円です"
   end
 end
 
